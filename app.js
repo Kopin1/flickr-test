7120f1299e91a11ff8f6b59842a27bdb4cc63cfc
Konstantin Pinaev <konstantin.pinaev@wunderkraut.se>
2019-01-06 13:47:53 +0100

    More ajustments

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const $ = jQuery;
+
 // Example of how to make an HTTP request in jQuery.
 let api = 'https://api.flickr.com/services/feeds/photos_public.gne';
 
@@ -14,7 +16,7 @@ $.ajax({
   dataType: 'jsonp',
 });
 
-// Get the data from API call.
+// Callback for the the data from API call.
 function getJsonData(data){
   // Do something.
   console.log(data);
